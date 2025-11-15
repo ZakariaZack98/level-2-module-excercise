5f8a8a542ca95baa20f8e9241c95f5b59c17a9af
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-11-15 22:45:37 +0600

    interface and type assertion

--- src/ts1.ts
+++ src/ts1.ts
@@ -1,3 +1,40 @@
-const newString:string = 'Hello world';
+interface User {
+  name: string;
+  age: number
+}
+interface Designation {
+  position: string;
+  location: string;
+}
 
-console.log(newString);
+interface User extends Designation {
+  
+}
+
+const userWithRole: User = {
+  name: 'zakaria',
+  age: 26,
+  position: 'sr. dev',
+  location: 'dhaka'
+}
+
+type ModUser = {
+  name: string;
+  userprop: string
+}
+
+type Role = {
+  role: string;
+  roleprop: string
+}
+
+type ModUserWithRole = ModUser | Role;
+
+const newUser: ModUserWithRole = {
+  name: 'zack',
+  userprop: '2',
+  role: 'dev',
+  roleprop: '3'
+}
+
+
